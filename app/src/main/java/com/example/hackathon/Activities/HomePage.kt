1207eac1066302185bbfaf78280469c4281eaf58
prooheckcp <vasco.soares.2001@gmail.com>
2021-01-18 23:41:39 +0000

    Update

--- app/src/main/java/com/example/hackathon/Activities/HomePage.kt
+++ app/src/main/java/com/example/hackathon/Activities/HomePage.kt
@@ -2,11 +2,47 @@ package com.example.hackathon.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.hackathon.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+
+        
+
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.page_1 -> {
+                    //Respond to the main homepage
+                    true
+                }
+                R.id.page_2 -> {
+                    // Respond to the notes tab
+                    true
+                }
+                R.id.page_3 ->{
+                    //Respond to settings tab
+
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+
     }
+
+    private fun fragmentManager(fragment: Fragment):Unit{
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.main_fragment_content, fragment)
+            commit()
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/hackathon/Activities/MainActivity.kt
+++ app/src/main/java/com/example/hackathon/Activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.hackathon.Activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hackathon.R
@@ -9,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        startActivity(Intent(this, HomePage::class.java))
     }
 
 
