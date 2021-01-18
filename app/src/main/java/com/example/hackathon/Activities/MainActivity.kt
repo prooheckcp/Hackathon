88da472f84928c51245ad5d8a3f892662e3cc5d4
prooheckcp <vasco.soares.2001@gmail.com>
2021-01-18 21:13:39 +0000

    DUDUDU

--- app/src/main/java/com/example/hackathon/Activities/MainActivity.kt
+++ app/src/main/java/com/example/hackathon/Activities/MainActivity.kt
@@ -1,11 +1,16 @@
-package com.example.hackathon
+package com.example.hackathon.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hackathon.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
+
+
 }
