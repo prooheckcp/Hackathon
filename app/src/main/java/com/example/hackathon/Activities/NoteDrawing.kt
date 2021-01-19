331c991446315b3b6efd3d62795d71ea6cf2e387
prooheckcp <vasco.soares.2001@gmail.com>
2021-01-19 20:58:42 +0000

    Added drawing canvas

--- app/src/main/java/com/example/hackathon/Activities/NoteDrawing.kt
+++ app/src/main/java/com/example/hackathon/Activities/NoteDrawing.kt
@@ -2,11 +2,15 @@ package com.example.hackathon.Activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hackathon.Objects.DataHandler
 import com.example.hackathon.R
+import kotlinx.android.synthetic.main.activity_note_drawing.*
 
 class NoteDrawing : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_note_drawing)
+        
+        noteName.text = DataHandler.currentNote?.name
     }
 }
