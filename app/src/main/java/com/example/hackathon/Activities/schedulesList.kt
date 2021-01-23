087b11007e9bdac818e87f7e17e0c914093c37a9
prooheckcp <vasco.soares.2001@gmail.com>
2021-01-23 17:08:29 +0000

    LINDO

--- app/src/main/java/com/example/hackathon/Activities/schedulesList.kt
+++ app/src/main/java/com/example/hackathon/Activities/schedulesList.kt
@@ -1,14 +1,19 @@
 package com.example.hackathon.Activities
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hackathon.Adapters.classesAdapter
 import com.example.hackathon.Adapters.weekdayAdapter
 import com.example.hackathon.Classes.scheduleClass
+import com.example.hackathon.Fragments.Notes
+import com.example.hackathon.Fragments.dummy.DummyContent
 import com.example.hackathon.Objects.DataHandler
 import com.example.hackathon.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import kotlinx.android.synthetic.main.activity_schedules_list.*
 import kotlinx.android.synthetic.main.activity_week_days.*
 
@@ -36,6 +41,46 @@ class schedulesList : AppCompatActivity() {
         goBackButtonSchedule.setOnClickListener {
             val intent = Intent(this, weekDays::class.java)
             startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+
+        //Handle the views used for the input
+        val nameInput = EditText(this)
+        nameInput.hint = "Insert class name"
+        nameInput.setTextColor(Color.BLACK)
+        nameInput.textSize = 16f
+
+        val linkInput = EditText(this)
+        linkInput.hint = "Insert classroom link"
+        linkInput.setTextColor(Color.BLACK)
+        linkInput.textSize = 16f
+
+
+        /*val picker =  MaterialTimePicker.Builder()
+                        .setTimeFormat(TimeFormat.CLOCK_12H)
+                        .setHour(12)
+                        .setMinute(10)
+                        .setTitle("Select Appointment time")
+                        .build()*/
+
+        //Clicked to add a classroom
+        addClassButton.setOnClickListener {
+
+            //Create the dialog box for the note||
+            var newNoteBox = MaterialAlertDialogBuilder(this)
+            newNoteBox.setTitle("New class")
+            newNoteBox.setView(nameInput)
+            newNoteBox.setMessage("Do you wish to add a new class?")
+            newNoteBox.setPositiveButton("Create"){dialog, which ->
+
+            }
+            newNoteBox.setNegativeButton("Cancel"){_,_->}
+            //__________________________________||
+            newNoteBox.show()
         }
     }
 
